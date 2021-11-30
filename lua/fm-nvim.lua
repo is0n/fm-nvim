6faf6443116641f543c5ebad7a25f5f7842c4ca4
Luis Palacios <paluisito2007@gmail.com>
2021-11-29 17:33:10 -0800

    Fix broot_cmd issue and update floating window function

--- lua/fm-nvim.lua
+++ lua/fm-nvim.lua
@@ -58,18 +58,18 @@ local function on_exit()
 end
 
 local function createWin(cmd, suffix)
-	local Buf = vim.api.nvim_create_buf(false, true)
+	Buf = vim.api.nvim_create_buf(false, true)
 	local win_height = math.ceil(vim.api.nvim_get_option("lines") * config.height - 4)
 	local win_width = math.ceil(vim.api.nvim_get_option("columns") * config.width)
 	local row = math.ceil((vim.api.nvim_get_option("lines") - win_height) / 2 - 1)
 	local col = math.ceil((vim.api.nvim_get_option("columns") - win_width) / 2)
 	local opts = { style = "minimal", relative = "editor", border = config.border, width = win_width, height = win_height, row = row, col = col }
-	local Win = vim.api.nvim_open_win(Buf, true, opts)
+	Win = vim.api.nvim_open_win(Buf, true, opts)
 	vim.fn.termopen(cmd, { on_exit = on_exit })
 	vim.api.nvim_command("startinsert")
 	vim.api.nvim_win_set_option(Win, 'winhl', 'Normal:' .. config.float_hl .. ',FloatBorder:' .. config.border_hl)
 	vim.api.nvim_win_set_option(Win, 'winblend', config.blend)
-	vim.api.nvim_buf_set_option(Buf, 'filetype', 'fm-nvim')
+	vim.api.nvim_buf_set_option(Buf, 'filetype', 'Fm')
 	vim.api.nvim_buf_set_keymap(Buf, 't', config.mappings.edit, '<C-\\><C-n>:lua require("fm-nvim").setMethod("edit")<CR>i' .. suffix, { silent = true })
 	vim.api.nvim_buf_set_keymap(Buf, 't', config.mappings.tabedit, '<C-\\><C-n>:lua require("fm-nvim").setMethod("tabedit")<CR>i' .. suffix, { silent = true })
 	vim.api.nvim_buf_set_keymap(Buf, 't', config.mappings.horz_split, '<C-\\><C-n>:lua require("fm-nvim").setMethod("split | edit")<CR>i' .. suffix, { silent = true })
@@ -88,7 +88,7 @@ function M.Fzy() createWin(config.cmds.fzy_cmd .. " > /tmp/fm-nvim", "<CR>") end
 function M.Xplr(dir) dir = dir or "." createWin(config.cmds.xplr_cmd .. " > /tmp/fm-nvim " .. dir, "<CR>") end
 function M.Vifm(dir) dir = dir or "." createWin(config.cmds.vifm_cmd .. " --choose-files /tmp/fm-nvim " .. dir, "l") end
 function M.Skim() createWin(config.cmds.skim_cmd .. " > /tmp/fm-nvim", "<CR>") end
-function M.Broot(dir) dir = dir or "." createWin("broot --conf " .. vim.fn.stdpath("data") .. "/site/pack/packer/start/fm-nvim/assets/broot_conf.hjson --out /tmp/fm-nvim " .. dir, "<CR>") end
+function M.Broot(dir) dir = dir or "." createWin(config.cmds.broot_cmd .. " --conf " .. vim.fn.stdpath("data") .. "/site/pack/packer/start/fm-nvim/assets/broot_conf.hjson --out /tmp/fm-nvim " .. dir, "<CR>") end
 function M.Ranger(dir) dir = dir or "." createWin(config.cmds.ranger_cmd .. " --choosefiles=/tmp/fm-nvim " .. dir, "l") end
 function M.Joshuto(dir) dir = dir or "." createWin(config.cmds.joshuto_cmd .. " --choosefiles /tmp/fm-nvim --path " .. dir, "l") end
 
