26b46ce26bba41e71b59767a7ef8628120b458c9
Luis Palacios <paluisito2007@gmail.com>
2021-12-17 19:20:20 -0800

    Update comamnds

--- plugin/fm-nvim.lua
+++ plugin/fm-nvim.lua
@@ -1,15 +1,9 @@
-vim.cmd [[ command! -nargs=? -complete=dir Lf :lua require('fm-nvim').Lf(<f-args>) ]]
-vim.cmd [[ command! -nargs=? -complete=dir Fm :lua require('fm-nvim').Fm(<f-args>) ]]
-vim.cmd [[ command! -nargs=? -complete=dir Nnn :lua require('fm-nvim').Nnn(<f-args>) ]]
-vim.cmd [[ command! -nargs=? -complete=dir Fff :lua require('fm-nvim').Fff(<f-args>) ]]
-vim.cmd [[ command! -nargs=? -complete=dir Twf :lua require('fm-nvim').Twf(<f-args>) ]]
+local fileManagers = { "Lf", "Fm", "Nnn", "Fff", "Twf", "Xplr", "Vifm", "Broot", "Gitui", "Ranger", "Joshuto", "Lazygit" }
+
+for _, fm in ipairs(fileManagers) do
+	vim.cmd("command! -nargs=? -complete=dir " .. fm .. " :lua require('fm-nvim')." .. fm .. "(<f-args>)")
+end
+
 vim.cmd [[ command! Fzf :lua require('fm-nvim').Fzf() ]]
 vim.cmd [[ command! Fzy :lua require('fm-nvim').Fzy() ]]
-vim.cmd [[ command! -nargs=? -complete=dir Xplr :lua require('fm-nvim').Xplr(<f-args>) ]]
-vim.cmd [[ command! -nargs=? -complete=dir Vifm :lua require('fm-nvim').Vifm(<f-args>) ]]
 vim.cmd [[ command! Skim :lua require('fm-nvim').Skim() ]]
-vim.cmd [[ command! -nargs=? -complete=dir Broot :lua require('fm-nvim').Broot(<f-args>) ]]
-vim.cmd [[ command! -nargs=? -complete=dir Gitui :lua require('fm-nvim').Gitui(<f-args>) ]]
-vim.cmd [[ command! -nargs=? -complete=dir Ranger :lua require('fm-nvim').Ranger(<f-args>) ]]
-vim.cmd [[ command! -nargs=? -complete=dir Joshuto :lua require('fm-nvim').Joshuto(<f-args>) ]]
-vim.cmd [[ command! -nargs=? -complete=dir Lazygit :lua require('fm-nvim').Lazygit(<f-args>) ]]
