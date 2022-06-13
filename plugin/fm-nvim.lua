b176d06fa6000632e956179b3bfa5e3f9ed091df
saccarosium <github.e41mv@aleeas.com>
2022-06-13 16:46:37 +0200

    fix: create commands only if executable is installed

--- plugin/fm-nvim.lua
+++ plugin/fm-nvim.lua
@@ -1,10 +1,24 @@
 local fileManagers = {"Lf", "Fm", "Nnn", "Fff", "Twf", "Xplr", "Vifm", "Broot", "Gitui", "Ranger", "Joshuto", "Lazygit"}
+local executable = vim.fn.executable
 
 for _, fm in ipairs(fileManagers) do
-    vim.cmd("command! -nargs=? -complete=dir " .. fm .. " :lua require('fm-nvim')." .. fm .. "(<f-args>)")
+    if executable(vim.fn.tolower(fm)) == 1 then
+        vim.cmd("command! -nargs=? -complete=dir " .. fm .. " :lua require('fm-nvim')." .. fm .. "(<f-args>)")
+    end
 end
 
-vim.cmd [[ command! Fzf :lua require('fm-nvim').Fzf() ]]
-vim.cmd [[ command! Fzy :lua require('fm-nvim').Fzy() ]]
-vim.cmd [[ command! Skim :lua require('fm-nvim').Skim() ]]
-vim.cmd [[ command! Neomutt :lua require('fm-nvim').Neomutt() ]]
+if executable("fzf") == 1 then
+    vim.cmd [[ command! Fzf :lua require('fm-nvim').Fzf() ]]
+end
+
+if executable("fzy") == 1 then
+    vim.cmd [[ command! Fzy :lua require('fm-nvim').Fzy() ]]
+end
+
+if executable("sk") == 1 then
+    vim.cmd [[ command! Skim :lua require('fm-nvim').Skim() ]]
+end
+
+if executable("neomutt") == 1 then
+    vim.cmd [[ command! Neomutt :lua require('fm-nvim').Neomutt() ]]
+end
